9aadb5bddf53b5101feefe7e996a6073bca77356
mandemeskel <mandemeskel@gmail.com>
2016-03-22 18:21:52 -0700

    landing page styles clean up
    such a SUNNY DAYYY!!!

--- header.php
+++ header.php
@@ -40,7 +40,7 @@
 					<!-- /nav -->
 
 					<!-- msg -->
-					<div class="landing_msg sunny_day">
+					<div class="landing_msg">
 						<h1> <?php bloginfo( 'name' ); ?> </h1>
 						<h2> <?php bloginfo( 'description' ); ?> </h2>
 					</div>
